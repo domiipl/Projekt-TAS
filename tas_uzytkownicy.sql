b43c64786dc7225362c89c6b43dd3bf5b546d487
Patryk Dąbrowski <lagakappa@gmail.com>
2018-10-29 20:13:31 +0100

    Ustandaryzowanie tabel + propozycja lepszych rozwiązań w tabelach

--- tas_uzytkownicy.sql
+++ tas_uzytkownicy.sql
@@ -1,55 +1,12 @@
--- MySQL dump 10.13  Distrib 8.0.13, for Win64 (x86_64)
---
--- Host: localhost    Database: tas
--- ------------------------------------------------------
--- Server version	8.0.13
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
- SET NAMES utf8 ;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `uzytkownicy`
---
-
-DROP TABLE IF EXISTS `uzytkownicy`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
 CREATE TABLE `uzytkownicy` (
-  `id_uzytkownika` int(11) NOT NULL AUTO_INCREMENT,
-  `login` varchar(15) NOT NULL,
-  `haslo` varchar(45) NOT NULL,
-  `email` varchar(45) NOT NULL,
-  PRIMARY KEY (`id_uzytkownika`),
-  UNIQUE KEY `login_UNIQUE` (`login`),
-  UNIQUE KEY `email_UNIQUE` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `uzytkownicy`
---
-
-LOCK TABLES `uzytkownicy` WRITE;
-/*!40000 ALTER TABLE `uzytkownicy` DISABLE KEYS */;
-INSERT INTO `uzytkownicy` VALUES (1,'test','haselko','test@gmail.com'),(2,'pierwszy','naklejka','pierwszy@gmail.com');
-/*!40000 ALTER TABLE `uzytkownicy` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2018-10-29 15:57:30
+	`id_uzytkownika` INT(11) NOT NULL AUTO_INCREMENT,
+	`login` VARCHAR(32) NOT NULL,
+	`haslo` VARCHAR(64) NOT NULL,
+	`email` VARCHAR(128) NOT NULL,
+	PRIMARY KEY (`id_uzytkownika`),
+	UNIQUE INDEX `login_UNIQUE` (`login`),
+	UNIQUE INDEX `email_UNIQUE` (`email`)
+)
+COLLATE='utf8mb4_general_ci'
+ENGINE=InnoDB
+;
